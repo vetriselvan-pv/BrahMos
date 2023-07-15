8cc6686a922002aa703ddf69ecf8622fa6834d6d
vetriselvan-pv <learnerkid11@gmail.com>
2023-07-15 11:31:27 +0530

    feat(react-framework): initial added

--- apps/hotel-management/src/app/app.tsx
+++ apps/hotel-management/src/app/app.tsx
@@ -1,55 +1,61 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
 
+import useFormControl from './formcontrol';
 import NxWelcome from './nx-welcome';
 
 import { Route, Routes, Link } from 'react-router-dom';
 
 export function App() {
-  return (
-    <>
-      <NxWelcome title="hotel-management" />
+    const { takeRef } = useFormControl();
+    return (
+        <>
+            <form>
+                <input type="text" name="text" ref={takeRef} />
+            </form>
+            <NxWelcome title="hotel-management" />
 
-      <div />
+            <div />
 
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
+            {/* START: routes */}
+            {/* These routes and navigation have been generated for you */}
+            {/* Feel free to move and update them to fit your needs */}
+            <br />
+            <hr />
+            <br />
+            <div role="navigation">
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/page-2">Page 2</Link>
+                    </li>
+                </ul>
             </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
-    </>
-  );
+            <Routes>
+                <Route
+                    path="/"
+                    element={
+                        <div>
+                            This is the generated root route.{' '}
+                            <Link to="/page-2">Click here for page 2.</Link>
+                        </div>
+                    }
+                />
+                <Route
+                    path="/page-2"
+                    element={
+                        <div>
+                            <Link to="/">
+                                Click here to go back to root page.
+                            </Link>
+                        </div>
+                    }
+                />
+            </Routes>
+            {/* END: routes */}
+        </>
+    );
 }
 
 export default App;
